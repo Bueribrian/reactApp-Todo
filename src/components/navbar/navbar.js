74b0a4b98a7ff92e4a663d4e032b5b2361bc603f
Brian Bueri <32884323+Bueribrian@users.noreply.github.com>
2019-05-29 18:29:36 -0300

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,6 +5,7 @@ class NavbarPage extends Component {
 render() {
   return (
       <MDBNavbar color="unique-color-dark py-3"  dark>
+        <h1>TaskManager</h1>
         <h2 className='text-white h5 p-0 m-0'>
         Notas  <MDBBadge>{this.props.notes.length} </MDBBadge>
         <span className='ml-5 text-gray h5'> Notas completadas <MDBBadge>{this.props.noteFinished.length}</MDBBadge></span>
